4c737d87bca55a692c74aeead0d61425facd42cc
benholcomb23 <ben.holcomb5@gmail.com>
2021-05-06 13:02:15 -0400

    "Twerrrk" message
    Created an 'if' statement that messages user if they enter the Twerrrk channel

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected, sheeeeeeeeeshh!')
 
-
-
 work_words = ['work', 'working', 'slack', 'qualcomm', 'twerrrk']
 
 stop_working_responses = [
@@ -22,6 +20,9 @@ stop_working_responses = [
     "You should be smashing, not working",
     "Do not lust after productivity, my friend!"
 ]
+
+
+# messages user to stop working if they send message about work
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -29,11 +30,24 @@ async def on_message(message):
 
     msg = message.content
 
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
     if any(word in msg.lower() for word in work_words):
         await message.channel.send(random.choice(stop_working_responses))
+
+
+# messages a user if they enter the "Twerrrk" channel
+@client.event
+async def on_voice_state_update(user, before, after):
+
+    entered_twerrrk_responses = [
+        f'bro, @{user.nick}, why u on this channel... 😐😐😐',
+        f'hey everyone, @{user.nick} is WORKING',
+        f'@{user.nick}. pls quit your job',
+        f'not even Tyler works as much as @{user.nick}'
+    ]
+
+    if before.channel is None and after.channel is not None:
+        if after.channel.id == 692913282457141399:
+            ctx_for_voice_chat = client.get_channel(773948529902223380)
+            await ctx_for_voice_chat.send(random.choice(entered_twerrrk_responses))
 
 client.run(token)
