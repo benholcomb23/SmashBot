9748670b28fd2a0b5cde8024459fa74e8388ee16
benholcomb23 <ben.holcomb5@gmail.com>
2021-05-06 22:17:26 -0400

    pinginguser

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import os
 import random
 import discord
+import responses
+from string import Template
 
 # env
 token = os.environ.get('DISCORD_TOKEN')
@@ -15,12 +17,6 @@ async def on_ready():
 
 work_words = ['work', 'working', 'slack', 'qualcomm', 'twerrrk']
 
-stop_working_responses = [
-    "Stop working.",
-    "You should be smashing, not working",
-    "Do not lust after productivity, my friend!"
-]
-
 
 # messages user to stop working if they send message about work
 @client.event
@@ -31,23 +27,17 @@ async def on_message(message):
     msg = message.content
 
     if any(word in msg.lower() for word in work_words):
-        await message.channel.send(random.choice(stop_working_responses))
+        await message.channel.send(random.choice(responses.stop_working_responses))
 
 
 # messages a user if they enter the "Twerrrk" channel
 @client.event
 async def on_voice_state_update(user, before, after):
-
-    entered_twerrrk_responses = [
-        f'bro, {user.mention}, why u on this channel... 😐😐😐',
-        f'hey everyone, {user.mention} is WORKING',
-        f'{user.mention}. pls quit your job',
-        f'not even Tyler works as much as {user.mention}'
-    ]
-
     if before.channel is None and after.channel is not None:
         if after.channel.id == 692913282457141399:
+            worker = user.mention
             ctx_for_voice_chat = client.get_channel(773948529902223380)
-            await ctx_for_voice_chat.send(random.choice(entered_twerrrk_responses))
+            await ctx_for_voice_chat.send(Template(random.choice(responses.entered_twerrrk_responses)).substitute(
+                worker=worker))
 
 client.run(token)
---
+++ venv/responses.py
@@ -0,0 +1,15 @@
+import discord
+import os
+
+stop_working_responses = [
+    "Stop working.",
+    "You should be smashing, not working",
+    "Do not lust after productivity, my friend!"
+]
+
+entered_twerrrk_responses = [
+    f'bro, $worker, why u on this channel... 😐😐😐',
+    f'hey everyone, $worker is WORKING',
+    f'$worker. pls quit your job',
+    f'not even Tyler works as much as $worker'
+]
